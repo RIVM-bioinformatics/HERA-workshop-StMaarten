cb45393e51fca2c255f64f610af8e10f595a9471
Florian Zwagemaker <f.zwagemaker@gmail.com>
2024-03-12 08:02:25 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -4,9 +4,8 @@
 rm -rf /content/sample_data
 mkdir -p input_files
 mkdir -p pipeline_output
-mkdir -p source
 mkdir -p output_data/alignments output_data/adapter_removal output_data/quality_control output_data/primer_removal
-bash -c "$(wget -q https://github.com/RIVM-bioinformatics/HERA-workshop-StMaarten/tarball/main -O - | tar -xz -C source/ --strip-components=1)"
+bash -c "$(wget -q https://github.com/RIVM-bioinformatics/HERA-workshop-StMaarten/tarball/main -O - | tar -xz -C ./ --strip-components=1)"
 
 # get sample fastq file from ENA
 wget -q https://surfdrive.surf.nl/files/index.php/s/KwTJFvZBklNhSzU/download -O input_files/samples.zip && unzip input_files/samples.zip -d input_files > /dev/null 2>&1 && rm input_files/samples.zip
@@ -28,6 +27,15 @@ echo "Installing and preparing source material, this may take a while..."
 mamba create -n pipeline_env viroconstrictor -c bioconda -y > /dev/null 2>&1
 
 pip install igv-jupyter --quiet > /dev/null 2>&1
+
+cat <<EOF >> ~/.ViroConstrictor_defaultprofile.ini
+[COMPUTING]
+compmode = local
+
+[GENERAL]
+auto_update = no
+ask_for_update = no
+EOF
 
 
 echo -e "\e[1m\e[32mDone with installing everything, you can now continue with the rest of the course.\e[39m"
