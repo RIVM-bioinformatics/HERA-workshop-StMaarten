9031d2b2694e316073a53ab2f98e2ccdec436a62
Florian Zwagemaker <f.zwagemaker@gmail.com>
2024-03-12 07:43:27 -0400

    Update setup, add section for IQtree, add initial colab version

--- setup.sh
+++ setup.sh
@@ -2,38 +2,30 @@
 
 # Remove standard directories placed as a default by Colab
 rm -rf /content/sample_data
-mkdir -p example_data
-mkdir -p source/
+mkdir -p input_files
+mkdir -p pipeline_output
+mkdir -p source
 mkdir -p output_data/alignments output_data/adapter_removal output_data/quality_control output_data/primer_removal
-bash -c "$(wget -q https://github.com/RIVM-bioinformatics/HERA-Bioinformatics-Training/tarball/main -O - | tar -xz -C source/ --strip-components=1)"
+bash -c "$(wget -q https://github.com/RIVM-bioinformatics/HERA-workshop-StMaarten/tarball/main -O - | tar -xz -C source/ --strip-components=1)"
 
 # get sample fastq file from ENA
-wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR990/007/ERR9900947/ERR9900947.fastq.gz -O example_data/nanopore_fastq.fastq.gz & 
-wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR408/008/ERR4082808/ERR4082808_1.fastq.gz -O example_data/illumina_fastq_1.fastq.gz &
-wget -q ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR408/008/ERR4082808/ERR4082808_2.fastq.gz -O example_data/illumina_fastq_2.fastq.gz &
+wget -q https://surfdrive.surf.nl/files/index.php/s/KwTJFvZBklNhSzU/download -O input_files/samples.zip && unzip input_files/samples.zip -d input_files > /dev/null 2>&1 && rm input_files/samples.zip
+wget -q https://surfdrive.surf.nl/files/index.php/s/Jcia7L8HWsKohBK/download -O data.tar.gz && tar xzf data.tar.gz && rm data.tar.gz
+
+rm -rf pipeline_output/results pipeline_output/Runinfomation.pdf pipeline_output/data/Virus~sars-cov-2/RefID~MN908947.3/alignment pipeline_output/data/Virus~sars-cov-2/RefID~MN908947.3/consensus
 
 ## install miniconda and get some basic tools
-wget -q https://repo.anaconda.com/miniconda/Miniconda3-py37_4.11.0-Linux-x86_64.sh
-chmod +x Miniconda3-py37_4.11.0-Linux-x86_64.sh
-./Miniconda3-py37_4.11.0-Linux-x86_64.sh -b -f -p /usr/local > /dev/null 2>&1
-rm Miniconda3-py37_4.11.0-Linux-x86_64.sh
+wget -q "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh -b -f -p /usr/local > /dev/null 2>&1
 
 # setup the necessary channels
-conda config --add channels anaconda
-conda config --add channels intel
-conda config --add channels conda-forge
-conda config --add channels bioconda
-
-# install mamba as a base package on the VM
-conda install -q mamba==0.24.0 python=3.8 pip -y > /dev/null 2>&1
+# mamba config --add channels bioconda
 
 echo "Conda is installed as well as some fundamentals"
 echo "Installing and preparing source material, this may take a while..."
 
 # make a dedicated environment for every course section
-mamba create -n Data_cleanup ampligone fastp -y > /dev/null 2>&1
-mamba create -n Alignments minimap2 samtools bedtools pysam -y >/dev/null 2>&1
-mamba create -n Consensus_seq longshot medaka bcftools pip -y >/dev/null 2>&1; source activate Consensus_seq; pip install git+https://github.com/RIVM-bioinformatics/TrueConsense.git@rewrite > /dev/null 2>&1; conda deactivate > /dev/null 2>&1
+mamba create -n pipeline_env viroconstrictor -c bioconda -y > /dev/null 2>&1
 
 pip install igv-jupyter --quiet > /dev/null 2>&1
 
